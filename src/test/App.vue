31271c88a411c5df3df86eb1763edb34619bf104
yonava <yonavossandreae@gmail.com>
2025-12-26 17:27:48 -0700

    tw

---
+++ src/test/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useMagicCanvas } from '..';
+
+
+const magic = useMagicCanvas()
+
+magic.draw.backgroundPattern.value = (ctx, at, alpha) => {
+  ctx.fillStyle = alpha ? `rgba(255, 0, 0, ${Number(alpha) / 100})` : 'red';
+  ctx.fillRect(at.x, at.y, 10, 10);
+}
+</script>
+
+<template>
+  <MagicCanvas class="bg-amber-400" v-bind="magic.ref" />
+</template>
---
+++ src/test/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { MagicCanvasPlugin } from "..";
+createApp(App).use(MagicCanvasPlugin).mount("#app");
